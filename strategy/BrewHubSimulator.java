eb3b3003b8cccb0e31ee80dce139c7bb946f1ab3
Bryant <nuwebryant@gmail.com>
2026-09-22 21:36:29 +0300

    Uncomment simulation logic

--- strategy/BrewHubSimulator.java
+++ strategy/BrewHubSimulator.java
@@ -4,22 +4,22 @@ public class BrewHubSimulator{
     public static void main(String[] args){
 
         // Create order objects
-        // Order defaultOrder = new DefaultOrder();
-        // Order student = new StudentOrder();
-        // Order happyHourOrder  = new HappyHourOrder();
-        // Order loyaltyTierOrder = new LoyaltyTierOrder();
+        Order defaultOrder = new DefaultOrder();
+        Order student = new StudentOrder();
+        Order happyHourOrder  = new HappyHourOrder();
+        Order loyaltyTierOrder = new LoyaltyTierOrder();
 
-        // // Compute discounted prices
-        // double defaultPrice = defaultOrder.applyDiscount(200);
-        // double studentPrice = student.applyDiscount(200);
-        // double happyHourPrice = happyHourOrder.applyDiscount(200);
-        // double loyaltyPrice = loyaltyTierOrder.applyDiscount(200);
+        // Compute discounted prices
+        double defaultPrice = defaultOrder.applyDiscount(200);
+        double studentPrice = student.applyDiscount(200);
+        double happyHourPrice = happyHourOrder.applyDiscount(200);
+        double loyaltyPrice = loyaltyTierOrder.applyDiscount(200);
 
 
-        // System.out.println("Your first purchase. This will cost $" + defaultPrice);
-        // System.out.println("Student discount applied. Total price is: $" + studentPrice + ".");
-        // System.out.println("It's Happy Hour!! Coffee costs $" + happyHourPrice);
-        // System.out.println("You're a loyal customer. Your coffee will cost $" + loyaltyPrice);
+        System.out.println("Your first purchase. This will cost $" + defaultPrice);
+        System.out.println("Student discount applied. Total price is: $" + studentPrice + ".");
+        System.out.println("It's Happy Hour!! Coffee costs $" + happyHourPrice);
+        System.out.println("You're a loyal customer. Your coffee will cost $" + loyaltyPrice);
 
         Order seniorOrder = new SeniorCitizenOrder();
 
