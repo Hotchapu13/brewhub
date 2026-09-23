040b027194ccfcd07474b2794e4361ccfabe0714
Bryant <nuwebryant@gmail.com>
2026-09-23 15:49:20 +0300

    Implement concrete subject and observer classes

--- observer/Observer.java
+++ observer/Observer.java
@@ -1,3 +1,5 @@
+package observer;
+
 public interface Observer{
     public void update(boolean queued, boolean brewing, boolean ready);
 }
---
+++ observer/OrderStatusPublisher.java
@@ -0,0 +1,23 @@
+public class OrderStatusPublisher implements Subject{
+
+    @Override
+    public void registerObserver(Observer o) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'registerObserver'");
+    }
+
+    @Override
+    public void removeObserver(Observer o) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'removeObserver'");
+    }
+
+    @Override
+    public void notifyObserver(Observer o) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'notifyObserver'");
+    }
+
+    @Override 
+    
+}
